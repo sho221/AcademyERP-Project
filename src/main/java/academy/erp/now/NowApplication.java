37b91057942efbf39df86e31fdef6f3941f200ab
JIWON0813 <59535499+JIWON0813@users.noreply.github.com>
2020-11-16 21:19:20 +0900

    first commit

---
+++ src/main/java/academy/erp/now/NowApplication.java
@@ -0,0 +1,13 @@
+package academy.erp.now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/erp/now/NowApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.erp.now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
